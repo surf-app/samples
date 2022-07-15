168fc0f525f713e5bc30a86169d16e46e305643e
sebastiandanson <sebastian.danson@gmail.com>
2022-07-15 12:47:03 -0400

    SURF 822: Shopify integration to giveaways

---
+++ challenge-response/index.js
@@ -0,0 +1,46 @@
+//test url
+//http://127.0.0.1:5500/index.html?transaction_id=testtid&challenge_id=testcid&nonce=testnonce
+
+class GiveawayActions {
+  init() {
+    // set localstorage with params
+    storeQueryParameters();
+  }
+
+  actionCompleted() {
+    // Retrieve and log localstorage values
+    const surfParams = getQueryParameters();
+    console.log("Surf Params: %o", surfParams);
+
+    // eventually call API
+  }
+
+  getQueryParameters = () => {
+    return JSON.parse(localStorage.getItem("surfParams"));
+  };
+
+  storeQueryParameters = () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    const transactionId = urlParams.get("transaction_id");
+    const challengeId = urlParams.get("challenge_id");
+    const nonce = urlParams.get("nonce");
+
+    const params = {
+      transaction_id: transactionId,
+      challenge_id: challengeId,
+      nonce: nonce,
+    };
+
+    localStorage.setItem("surfParams", JSON.stringify(params));
+  };
+}
+
+// Somewhere outside the above class, the
+// site should call the following on page load
+const giveawayActions = new GiveawayActions();
+giveawayActions.init();
+
+// Then on the button click event, call:
+giveawayActions.actionCompleted();
